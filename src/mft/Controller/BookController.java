b20725311e2b2504a88caf4c60f344dbe2a24864
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-02 23:04:19 +0200

    commit

--- src/mft/Controller/BookController.java
+++ src/mft/Controller/BookController.java
@@ -1,4 +1,22 @@
 package mft.Controller;
 
-public class BookController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookController implements Initializable {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private Label title;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
