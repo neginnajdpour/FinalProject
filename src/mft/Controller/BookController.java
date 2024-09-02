ba5b6480e2c1f2ac19c314a52c359236288cb3c9
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-03 00:16:45 +0200

    commit

--- src/mft/Controller/BookController.java
+++ src/mft/Controller/BookController.java
@@ -2,7 +2,8 @@ package mft.Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ComboBox;
+import mft.Model.entity.Genre;
 
 import java.awt.*;
 import java.net.URL;
@@ -10,13 +11,36 @@ import java.util.ResourceBundle;
 
 public class BookController implements Initializable {
     @FXML
-    private TextField titleTxt,editionTxt ,authorTxt,publisherTxt,publicationyearTxt,availablecopiesTxt;
+    private TextField titleTxt;
 
     @FXML
-    private Label title;
+    private TextField editionTxt;
+
+    @FXML
+    private TextField authorTxt;
+
+    @FXML
+    private TextField publisherTxt;
+
+    @FXML
+    private TextField publicationyearTxt;
+
+    @FXML
+    private TextField availablecopiesTxt;
+
+    @FXML
+    private TextField descriptionTxt;
+
+    @FXML
+    private ComboBox genreCmb;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
+        for (Genre genre : Genre.values()) {
+            genreCmb.getItems().add(genre);
+        }
 
     }
 }
