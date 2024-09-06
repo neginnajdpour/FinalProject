ad92baee35fe73e854afea5207f77d620c3187bd
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-06 14:50:38 +0200

    commit

--- src/mft/Controller/MemberController.java
+++ src/mft/Controller/MemberController.java
@@ -1,16 +1,33 @@
 package mft.Controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import mft.Model.bl.MemberBl;
+import mft.Model.entity.Member;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class MemberController implements Initializable {
+
+    @FXML
+    private TextField firstnameTxt , lastnameTxt;
+
+    @FXML
+    private Button saveBtn;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        saveBtn.setOnAction(event -> {
+            Member member = new Member(firstnameTxt.getText(), lastnameTxt.getText());
+            MemberBl.save();
 
-
+        });
 
     }
 }
