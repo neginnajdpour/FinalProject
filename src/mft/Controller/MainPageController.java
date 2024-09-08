5d5941a4b9ddcf972a57ef001562431b0f3af9f8
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-08 23:27:58 +0200

    commit

--- src/mft/Controller/MainPageController.java
+++ src/mft/Controller/MainPageController.java
@@ -1,5 +1,6 @@
 package mft.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -10,9 +11,32 @@ import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
 
+    @FXML
+    private Menu resourcesMenu;
+
     
     @FXML
     private MenuItem addResourceMenu , resourceListMenu;
+
+    @FXML
+    private void handleAddResourceAction(ActionEvent event) {
+        System.out.println("addResourceAction");
+    }
+
+    @FXML
+    private void handleMiClose(ActionEvent event) {
+        System.out.println("handleMiClose");
+    }
+
+    @FXML
+    private void handleMiDelete(ActionEvent event) {
+        System.out.println("handleMiDelete");
+    }
+
+    @FXML
+    private void handleMIAbout(ActionEvent event) {
+        System.out.println("handleMIAbout");
+    }
 
 
 
@@ -20,7 +44,11 @@ public class MainPageController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        addResourceMenu.getO
+        MenuItem viewJobs = new MenuItem("Jobs");
+
+        Menu help = new Menu("Help");
+        MenuItem helpItem = new MenuItem("Help");
+        helpItem.setActionCommand("hello");
 
 
 
