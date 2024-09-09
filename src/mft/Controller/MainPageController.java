ea62caeac3fa0e8ca2d6eca6807dfb6a3bd63897
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-09 17:05:40 +0200

    commit

--- src/mft/Controller/MainPageController.java
+++ src/mft/Controller/MainPageController.java
@@ -2,11 +2,16 @@ package mft.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MainPageController implements Initializable {
@@ -19,8 +24,12 @@ public class MainPageController implements Initializable {
     private MenuItem addResourceMenu , resourceListMenu;
 
     @FXML
-    private void handleAddResourceAction(ActionEvent event) {
-        System.out.println("addResourceAction");
+    private void handleAddResourceAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Book.fxml"))));
+        stage.setTitle("Panel");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
