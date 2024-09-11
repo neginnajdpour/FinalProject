2ce4cd004a92f754b439cec9ff1b00e8054f6301
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-11 22:03:03 +0200

    commit

--- src/mft/Controller/BookSearchController.java
+++ src/mft/Controller/BookSearchController.java
@@ -3,16 +3,21 @@ package mft.Controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import lombok.SneakyThrows;
 import mft.Model.bl.ResourceBl;
 import mft.Model.da.FormState;
 import mft.Model.entity.Resource;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class BookSearchController implements Initializable {
@@ -59,6 +64,19 @@ public class BookSearchController implements Initializable {
         editBtn.setOnAction(event -> {
             Resource temp_resource = resourceTbl.getSelectionModel().getSelectedItem();
             FormState.resource = temp_resource;
+            Stage stage = new Stage();
+            Scene scene = null;
+            try {
+                scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/ResourceUpdate.fxml"))));
+                stage.setTitle("Panel");
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            stage.setScene(scene);
 
         });
 
--- src/mft/Controller/ResourceUpdateController.java
+++ src/mft/Controller/ResourceUpdateController.java
@@ -1,4 +1,45 @@
 package mft.Controller;
 
-public class ResourceUpdateController {
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import mft.Model.da.FormState;
+import mft.Model.entity.Resource;
+import mft.Model.entity.ResourceType;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ResourceUpdateController implements Initializable {
+
+    @FXML
+    private TextField isbnTxt, titleTxt, editionTxt, authorTxt, publisherTxt, quantityTxt;
+
+    @FXML
+    private TextArea descriptionTxt;
+
+    @FXML
+    private ComboBox resourcetypeCmb , categoryCmb, languageCmb;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Resource resource = new Resource();
+        resource = FormState.resource;
+
+        isbnTxt.setText(resource.getISBN());
+        titleTxt.setText(resource.getTITLE());
+        editionTxt.setText(resource.getEDITION());
+        authorTxt.setText(resource.getAUTHOR());
+        publisherTxt.setText(resource.getPUBLISHER());
+        quantityTxt.setText(String.valueOf(resource.getQUANTITY()));
+        descriptionTxt.setText(resource.getDESCRIPTION());
+        resourcetypeCmb.getSelectionModel().select(resource.getRESOURCE_TYPE());
+        categoryCmb.getSelectionModel().select(resource.getCATEGORY());
+        languageCmb.getSelectionModel().select(resource.getLANGUAGE());
+
+    }
 }
