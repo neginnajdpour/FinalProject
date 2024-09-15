26a71e19d191f6355c9865f2f430c5ca053fed1e
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-15 16:19:04 +0200

    commit

--- src/mft/Controller/MainPageController.java
+++ src/mft/Controller/MainPageController.java
@@ -18,41 +18,35 @@ import java.util.ResourceBundle;
 public class MainPageController implements Initializable {
 
     @FXML
-    private Menu resourcesMenu;
-
-    
-    @FXML
-    private MenuItem addResourceMenu , resourceListMenu;
+    private Button resourceBtn, memberBtn;
 
 
-    @SneakyThrows
-    @FXML
-    private void handleManageResourcesAction(ActionEvent event) {
-        Stage stage = new Stage();
-        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Resource.fxml"))));
-        stage.setTitle("Panel");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
-    @FXML
-    private void handleManageMembersAction(ActionEvent event) throws IOException {
-        Stage stage = new Stage();
-        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Member.fxml"))));
-        stage.setTitle("Panel");
-        stage.setScene(scene);
-        stage.show();
-    }
+//    @SneakyThrows
+//    @FXML
+//    private void handleManageResourcesAction(ActionEvent event) {
+//        Stage stage = new Stage();
+//        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Resource.fxml"))));
+//        stage.setTitle("Panel");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//
+//    @FXML
+//    private void handleManageMembersAction(ActionEvent event) throws IOException {
+//        Stage stage = new Stage();
+//        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Member.fxml"))));
+//        stage.setTitle("Panel");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        MenuItem viewJobs = new MenuItem("Jobs");
-        Menu help = new Menu("Help");
-        MenuItem helpItem = new MenuItem("Help");
-        helpItem.setActionCommand("hello");
+
+
 
     }
 }
