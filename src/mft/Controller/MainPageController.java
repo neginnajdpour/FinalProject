44d46a625ffa12e9cd5bdd5e02db43bd64f30a45
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-15 16:30:36 +0200

    commit

--- src/mft/Controller/MainPageController.java
+++ src/mft/Controller/MainPageController.java
@@ -5,9 +5,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lombok.SneakyThrows;
+import javafx.scene.control.*;
 
 import java.awt.*;
 import java.io.IOException;
@@ -44,6 +46,37 @@ public class MainPageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        resourceBtn.setOnAction(event -> {
+
+            try {
+                Stage stage = new Stage();
+                Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Resource.fxml"))));
+                stage.setTitle("Panel");
+                stage.setScene(scene);
+                stage.show();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        memberBtn.setOnAction(event -> {
+
+            try {
+                Stage stage = new Stage();
+                Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mft/View/Member.fxml"))));
+                stage.setTitle("Panel");
+                stage.setScene(scene);
+                stage.show();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
 
 
 
