7e3a540b6d4cda05422462d629498ac5d77f74ef
neginnajdpour <negin.najdpour@yahoo.com>
2024-09-22 19:40:20 +0200

    commit

--- src/mft/controller/SearchResourceController.java
+++ src/mft/controller/SearchResourceController.java
@@ -1,15 +1,72 @@
 package mft.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import mft.model.bl.MemberBl;
+import mft.model.entity.Member;
+import mft.model.entity.Resource;
+import mft.model.entity.ResourceType;
+import mft.model.entity.SearchCriteria;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class SearchResourceController implements Initializable {
+
+    @FXML
+    private ComboBox searchCmb;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private TextField searchTxt;
+
+    @FXML
+    private TableView<Resource> resourceTable;
+
+    @FXML
+    private TableColumn<Resource, String> titleCol , authorCol , isbnCol , publisherCol , editionCol , categoryCol , languageCol;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        for (SearchCriteria genre : SearchCriteria.values()) {
+            searchCmb.getItems().add(genre);
+        }
 
+        searchBtn.setOnAction(event -> {
+
+            String searchCriteria = searchCmb.getSelectionModel().getSelectedItem().toString();
+            if (searchCriteria == "RESOURCE_ID")
+            {
+                refreshResourceTbl(MemberBl.getAllResources(Integer.parseInt(searchTxt.getText())));
+            }
+        });
+
+
+
+
+
+
+    }
+
+    public void refreshResourceTbl(List<Member> resourceList) {
+
+        ObservableList<Member> observableList = FXCollections.observableList(resourceList);
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("nationalID"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+        isbnCol.setCellValueFactory(new PropertyValueFactory<>("LastName"));
+        publisherCol.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+        editionCol.setCellValueFactory(new PropertyValueFactory<>("Gender"));        ;
+        categoryCol.setCellValueFactory(new PropertyValueFactory<>("JoinDate"));
+        languageCol.setCellValueFactory(new PropertyValueFactory<>("Active"));
+        resourceTable.setItems(observableList);
 
     }
 }
--- src/mft/model/bl/ResourceBl.java
+++ src/mft/model/bl/ResourceBl.java
@@ -1,8 +1,13 @@
 package mft.model.bl;
 
 import mft.model.da.ResourceDa;
+import mft.model.entity.Category;
+import mft.model.entity.Language;
 import mft.model.entity.Resource;
+import mft.model.entity.ResourceType;
+import mft.model.tools.JdbcProvider;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +18,17 @@ public class ResourceBl {
         resourceDa.save(resource);
     }
 
+    public static void update(Resource resource) throws Exception {
+        ResourceDa resourceDa = new ResourceDa();
+        resourceDa.update(resource);
+    }
+
     public static void delete(Integer ISBN) throws Exception {
         ResourceDa resourceDa = new ResourceDa();
         resourceDa.delete(ISBN);
     }
 
-    public static void update(Resource resource) throws Exception {
-        ResourceDa resourceDa = new ResourceDa();
-        resourceDa.update(resource);
-    }
+
 
     public static Resource getResourceById(int resourceId) throws Exception {
         Resource resource = new Resource();
@@ -31,6 +38,13 @@ public class ResourceBl {
         return resource;
     }
 
+    public List<Resource> getResourcesByISBN(Integer ISBN) throws SQLException {
+        List<Resource> resources = new ArrayList<>();
+        ResourceDa resourceDa = new ResourceDa();
+        resources = resourceDa.getResourcesByISBN(ISBN);
+        return resources;
+    }
+
     public static List<Resource> getBooksByTitle(String resourceTitle) throws Exception {
         List<Resource> books = new ArrayList<>();
         ResourceDa resourceDa = new ResourceDa();
@@ -38,18 +52,40 @@ public class ResourceBl {
 
         return books;
     };
+    public List<Resource> getResourcesByResourceType(ResourceType resourceType) throws SQLException {
+        List<Resource> resources = new ArrayList<>();
+        ResourceDa resourceDa = new ResourceDa();
+        resources = resourceDa.getResourcesByResourceType(resourceType);
+        return resources;
+    }
 
+    public List<Resource> getResourcesByAuthor(String author) throws SQLException {
+        List<Resource> resources = new ArrayList<>();
+        ResourceDa resourceDa = new ResourceDa();
+        resources = resourceDa.getResourcesByAuthor(author);
+        return resources;
+    }
 
+    public List<Resource> getResourcesByPublisher(String publisher) throws SQLException {
+        List<Resource> resources = new ArrayList<>();
+        ResourceDa resourceDa = new ResourceDa();
+        resources = resourceDa.getResourcesByPublisher(publisher);
+        return resources;
+    }
 
-    public static List<Resource>  getAllResources() throws Exception {
+    public List<Resource> getResourcesByCategory(String category) throws SQLException {
+        List<Resource> resources = new ArrayList<>();
+        ResourceDa resourceDa = new ResourceDa();
+        resources = resourceDa.getResourcesByCategory(category);
+        return resources;
+    }
+
+    public List<Resource> getAllResources() throws SQLException {
         List<Resource> resources = new ArrayList<>();
         ResourceDa resourceDa = new ResourceDa();
         resources = resourceDa.getAllResources();
-
-//        List<Optional<Book>> optionalBooks  = books.stream()
-//                .map((o) -> Optional.of(o)).collect(Collectors.toList());
-
         return resources;
     }
+
 
 }
